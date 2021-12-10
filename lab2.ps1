1f7623d4a5d9322e69a4a564221f00259fb2bc3b
StardustDL <stardustdl@163.com>
2021-12-10 15:47:18 +0800

    add testall

--- lab2.ps1
+++ lab2.ps1
@@ -1,8 +1,11 @@
 $env:GOPATH=$PWD
 $env:GO111MODULE="auto"
 cd src/raft
+go test -run BasicAgree
+go test -run FailAgree
 go test -run FailNoAgree
 go test -run ConcurrentStarts
 go test -run Rejoin
 go test -run Backup
+go test -run Count
 cd ../..
