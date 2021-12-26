b4d8a272b1d92f78358195336b0b77e50aa642d0
StardustDL <stardustdl@163.com>
2021-12-26 16:07:52 +0800

    add multi-batch test script

---
+++ test.sh
@@ -0,0 +1,21 @@
+echo "Start"
+date
+file="$(date "+%Y-%m-%d-%H-%M-%S")"
+echo "Serial enable log"
+date
+python -u ./batch_test.py all -c 100 -w 1 > ./logs/$file-sl.log 2>&1
+cat ./logs/$file-sl.log | tail -n 17
+echo "Serial disable log"
+date
+python -u ./batch_test.py all -c 100 -f "" -w 1 > ./logs/$file-sn.log 2>&1
+cat ./logs/$file-sn.log | tail -n 17
+echo "Parallel enable log"
+date
+python -u ./batch_test.py all -c 1000 > ./logs/$file-pl.log 2>&1
+cat ./logs/$file-pl.log | tail -n 17
+echo ")arallel disable log"
+date
+python -u ./batch_test.py all -c 1000 -f "" > ./logs/$file-pn.log 2>&1
+cat ./logs/$file-pn.log | tail -n 17
+echo "Finish"
+date
