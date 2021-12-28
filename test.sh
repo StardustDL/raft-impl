979f2b4eb6e6a60e160f74073d6ff0e84edd0536
StardustDL <stardustdl@163.com>
2021-12-28 18:53:09 +0800

    fix test.sh cat

--- test.sh
+++ test.sh
@@ -5,14 +5,14 @@ exit 1;
 fi
 
 processors="$(cat /proc/cpuinfo | grep 'physical id' | wc -l)"
-workers="$(($processors*3))"
+workers="$(($processors*5))"
 file="$(date "+%Y-%m-%d-%H-%M-%S")"
 
 echo "Start Large Scale ($1 cases, $workers workers) (Parallel (Disabled Logging)) > ls-$file.log"
 LANG=en-us date
 
 python -u ./batch_test.py all -c $1 -w $workers -f "" > ./logs/ls-$file.log 2>&1
-cat ./logs/$file-ls.log | tail -n 17
+cat ./logs/ls-$file.log | tail -n 17
 
 echo ""
 echo "Finish"
